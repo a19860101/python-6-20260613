b87562db0156d964adab51bd1838f95227d85d96
Zac <a19860101@gmail.com>
2026-06-13 11:20:19 +0800

    20260613

---
+++ 0613/basic.py
@@ -0,0 +1,18 @@
+# integer 整數
+a = 1
+a2 = '1'
+b = 2
+# float 浮點數
+c = 3.14
+# string 字串
+d = 'hello'
+# boolean 布林
+e = True
+f = False
+
+# 檢查型別
+print(type(a))
+print(type(a2))
+print(type(c))
+print(type(f))
+
