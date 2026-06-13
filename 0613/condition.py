113349a0401aec37cb9a9ce424b4df84c1f76d95
Zac <a19860101@gmail.com>
2026-06-13 14:01:19 +0800

    20260613

--- 0613/condition.py
+++ 0613/condition.py
@@ -1,18 +1,40 @@
 # 條件式、判斷式
 
-x = -10
+x = 10
 # if
-# if x >= 10:
-#     print(x)
+
+# if x == 10:
+#     print(True)
 
 # if x > 10:
 #     print('success')
 # else:
 #     print('fail')
 
-if x > 10:
+# if x > 10:
+#     print('A+')
+# elif x > 5:
+#     print('A')
+# else:
+#     print('B')
+
+# > >= < <= == !=
+
+score = 100
+# 注意順序，如果用大於，要由大比到小；反之用小於，就由小比到大
+if score > 90:
     print('A+')
-elif x > 5:
+elif score > 80:
     print('A')
-else:
+elif score > 70:
     print('B')
+elif score > 60:
+    print('C')
+else:
+    print('D')
+
+day = 1
+if day == 0:
+    print('星期日')
+elif day == 1:
+    print('星期一')
