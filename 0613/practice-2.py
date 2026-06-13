cc69b611a8e93269fce859f475b65f418dc4f63a
Zac <a19860101@gmail.com>
2026-06-13 15:21:07 +0800

    20260613

---
+++ 0613/practice-2.py
@@ -0,0 +1,15 @@
+m = input('請選擇功能1)台幣轉美金 2)台幣轉日幣：')
+if m != '1' and m != '2':
+    exit('請輸入正確的功能')
+
+ntd = input('請輸入金額')
+if not ntd.isdigit():
+    exit('請輸入正確的數字')
+
+
+if m == '1':
+    result = int(ntd) / 32
+    print(f'台幣{ntd}約為美金{result}')
+elif m == '2':
+    result = int(ntd) / 0.198
+    print(f'台幣{ntd}約為日幣{result}')
