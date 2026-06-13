8931d0153b8b9428783dfec4aaeef8976f2dea55
Zac <a19860101@gmail.com>
2026-06-13 11:47:11 +0800

    20260613

--- 0613/basic.py
+++ 0613/basic.py
@@ -16,3 +16,13 @@ print(type(a2))
 print(type(c))
 print(type(f))
 
+# 變數命名規則
+# 變數名必須以字母或底線開頭。
+# 變數名不能以數字開頭。
+# 變數名稱只能包含字母數字字元和底線（Az、0-9 和 _）。
+# 變數名稱區分大小寫（age、Age 和 AGE 是三個不同的變數）。
+# 變數名不能是任何Python 關鍵字。
+
+# 關鍵字
+import keyword
+print(keyword.kwlist)
