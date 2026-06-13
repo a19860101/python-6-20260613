5d7271746e6397bac7623cbeb072b25e706eda20
Zac <a19860101@gmail.com>
2026-06-13 14:11:52 +0800

    20260613

---
+++ 0613/condition-match.py
@@ -0,0 +1,19 @@
+day = 6
+
+match day:
+    case 0:
+        print('星期日')
+    case 1:
+        print('星期一')
+    case 2:
+        print('星期二')
+    case 3:
+        print('星期三')
+    case 4:
+        print('星期四')
+    case 5:
+        print('星期五')
+    case 6:
+        print('星期六')
+    case _:
+        print('錯誤')
--- 0613/condition.py
+++ 0613/condition.py
@@ -33,8 +33,18 @@ elif score > 60:
 else:
     print('D')
 
-day = 1
+day = 6
 if day == 0:
     print('星期日')
 elif day == 1:
     print('星期一')
+elif day == 2:
+    print('星期二')
+elif day == 3:
+    print('星期三')
+elif day == 4:
+    print('星期四')
+elif day == 5:
+    print('星期五')
+elif day == 6:
+    print('星期六')
