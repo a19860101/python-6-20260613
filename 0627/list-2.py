29777de68a8098bf3277987ac4e0df00005e6654
Zac <a19860101@gmail.com>
2026-06-27 13:56:54 +0800

    20260627

---
+++ 0627/list-2.py
@@ -0,0 +1,19 @@
+# 串列方法
+
+fruits = ['apple','banana','mango']
+print(fruits)
+# fruits.append('orange')
+
+# fruits.insert(0,'orange')
+# 可迭代物件
+fruits.extend(['orange', 'kiwi','orange'])
+print(fruits)
+
+fruits.remove('orange')
+# del fruits[-1]
+
+# fruits.clear()
+
+fruits[2] = '芒果'
+
+print(fruits)
