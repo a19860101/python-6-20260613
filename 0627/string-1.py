504d89c0063ef9a2733224b0e4f1f3d395db8438
Zac <a19860101@gmail.com>
2026-06-27 10:49:50 +0800

    20260627

--- 0627/string-1.py
+++ 0627/string-1.py
@@ -1,21 +1,33 @@
 s = 'Hello World'
 
+
 # 字串切片
+# 字串[開始:結束:]
+# 字串[開始:結束:間距(step)]
+#[start: end: step]
 
-print(s)
 
-print(s[0])
-print(s[6])
-print(s[-1])
+# print(s)
+#
+# print(s[0])
+# print(s[6])
+# print(s[-1])
 
-print(s[3:8])
-
-print(s[0:5])
-print(s[6:11])
-
-print(s[:5])
-print(s[6:])
+# print(s[3:8])
+#
+# print(s[0:5])
+# print(s[6:11])
+#
+# print(s[:5])
+# print(s[6:])
 
 filename = 'qqq.jpg'
-print(filename[-3:])
-print(filename[:3])
+# print(filename[-3:])
+# print(filename[:3])
+
+n = '0123456789'
+print(n[::2])
+print(n[::3])
+print(n[::-1])
+
+print(s[::-1])
