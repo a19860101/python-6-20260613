9c070177a58676292ddea9483f013f32d0cf6d8b
Zac <a19860101@gmail.com>
2026-06-27 09:56:48 +0800

    20260627

--- 0627/loop.py
+++ 0627/loop.py
@@ -1,6 +1,13 @@
 # while
 
-x = 0
-while x < 10:
-    x = x + 1
-    print('Hello World')
+# x = 0
+# while x < 10:
+#     x = x + 1
+#     print('Hello World')
+
+# 不定數迴圈
+while True:
+    x = input('請輸入內容：')
+    print(x)
+    if x == 'q':
+        break
