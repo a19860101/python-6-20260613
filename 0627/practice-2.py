5f4d6672711dd4eeab40eb7d343e2637dd6bdb8e
Zac <a19860101@gmail.com>
2026-06-27 16:05:09 +0800

    20260627

--- 0627/practice-2.py
+++ 0627/practice-2.py
@@ -6,18 +6,22 @@ import random
 # print(random.randint(1,99))
 # print(random.randrange(1,10))
 # print(random.choice(q))
-
-ans = random.randint(1,10)
-print('猜數字遊戲開始')
-print(ans)
-
 while True:
-    guess = input('請輸入數字：')
-    guess = int(guess)
-    if guess > ans:
-        print('太大')
-    elif guess < ans:
-        print('太小')
-    else:
-        print('恭喜')
+    ans = random.randint(1,10)
+    print('猜數字遊戲開始')
+    print(ans)
+    while True:
+        guess = input('請輸入數字：')
+        guess = int(guess)
+        if guess > ans:
+            print('太大')
+        elif guess < ans:
+            print('太小')
+        else:
+            print('恭喜')
+            break
+
+    again = input('要再玩一場嗎？ y/n')
+    if again == 'n':
+        print('掰!下次見!!')
         break
