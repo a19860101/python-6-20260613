262fb0ef5faef0868d4f82977bb080bbbe7fc20f
Zac <a19860101@gmail.com>
2026-06-27 10:32:58 +0800

    20260627

---
+++ 0627/string-1.py
@@ -0,0 +1,21 @@
+s = 'Hello World'
+
+# 字串切片
+
+print(s)
+
+print(s[0])
+print(s[6])
+print(s[-1])
+
+print(s[3:8])
+
+print(s[0:5])
+print(s[6:11])
+
+print(s[:5])
+print(s[6:])
+
+filename = 'qqq.jpg'
+print(filename[-3:])
+print(filename[:3])
