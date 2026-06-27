02b685abec85590e2f5353627e7d041e1426a605
Zac <a19860101@gmail.com>
2026-06-27 12:25:54 +0800

    20260627

---
+++ 0627/list-1.py
@@ -0,0 +1,24 @@
+# list
+# dictionary
+# tuple
+# set
+
+# list串列
+lt1 = ['apple','banana','mango','pineapple','guava','coconut','kiwi']
+print(lt1)
+print(lt1[0])
+print(lt1[1])
+print(lt1[2])
+
+# 串列切片
+
+print(lt1[1:3])
+print(lt1[::2])
+print(lt1[::-1])
+
+# for item in range(3):
+#     print(lt1[item])
+
+for x in lt1:
+    print(x)
+
