4f6cc4adaf2ad8962cfbfbe19e28f67eb86657a9
Zac <a19860101@gmail.com>
2026-06-27 13:58:45 +0800

    20260627

--- 0627/list-2.py
+++ 0627/list-2.py
@@ -2,18 +2,27 @@
 
 fruits = ['apple','banana','mango']
 print(fruits)
+# append() 新增項目
 # fruits.append('orange')
 
+# insert() 在指定位置新增項目
 # fruits.insert(0,'orange')
+
+# extend() 新增可迭代物件
 # 可迭代物件
 fruits.extend(['orange', 'kiwi','orange'])
 print(fruits)
 
+# remove() 移除項目
 fruits.remove('orange')
+
+# del 透過索引移除項目
 # del fruits[-1]
 
+# clear() 清空串列
 # fruits.clear()
 
+# 修改串列
 fruits[2] = '芒果'
 
 print(fruits)
