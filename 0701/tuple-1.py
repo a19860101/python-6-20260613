f19da912001483c4451a0c51634342d15dac37e9
Zac <a19860101@gmail.com>
2026-07-04 11:32:01 +0800

    20260704

--- 0701/tuple-1.py
+++ 0701/tuple-1.py
@@ -1,10 +1,26 @@
 # 元組 tuple
 
-# t = ('apple','banana','orange')
+t = ('apple','banana','orange')
 # t = 'apple','banana','orange'
 # t = ('apple',)
-
+t2 = ('kiwi',)
 # t = ()
-t = tuple()
-
+# t = tuple()
 print(type(t))
+t3 = t + t2
+
+print(len(t3))
+print(t3.count('apple'))
+print(t3.index('apple'))
+
+print(t3[0])
+print(t3[-1])
+print(t3[1:3])
+print(t3[::2])
+print(t3[::-1])
+
+coord = (23.5, 121.2)
+lat, lon = coord
+
+print(lat, lon)
+
