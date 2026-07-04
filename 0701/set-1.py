743c361b1a731cb14e50b67e0b191534ae4a2fa3
Zac <a19860101@gmail.com>
2026-07-04 12:10:19 +0800

    20260704

--- 0701/set-1.py
+++ 0701/set-1.py
@@ -11,6 +11,25 @@ s1.discard(66)
 s1.update({123})
 s1.update({5})
 
-print(len(s1) )
+# print(len(s1) )
 
-print(s1)
+# print(s1)
+
+q1 = {'A','B','C','F'}
+q2 = {'A','B','E','F'}
+
+# 交集
+print(q1 & q2)
+print(q1.intersection(q2))
+# 聯集
+print(q1 | q2)
+print(q1.union(q2))
+# 差集
+print(q1 - q2)
+print(q2 - q1)
+print(q1.difference(q2))
+print(q2.difference(q1))
+
+# 對稱差集
+print(q1 ^ q2)
+print(q1.symmetric_difference(q2))
