ddbdd1b39585694ede9e053d918960e64a2aefa4
Zac <a19860101@gmail.com>
2026-07-04 10:05:27 +0800

    20260704

--- 0701/lotte.py
+++ 0701/lotte.py
@@ -1,19 +1,24 @@
 import random
 
-
 # 1
-result = []
-while True:
-    ans = random.randint(1, 49)
-    if ans in result:
-        continue
-    result.append(ans)
-    if len(result) == 6:
-        break
-
-print(result)
+# result = []
+# while True:
+#     ans = random.randint(1, 49)
+#     if ans in result:
+#         continue
+#     result.append(ans)
+#     if len(result) == 6:
+#         break
+#
+# print(result)
 
 # 2
 
-result = random.sample(range(1,49), k=6)
-print(result)
+# result = random.sample(range(1,49), k=6)
+# print(result)
+
+# 3
+cards = ['普通','稀有','超稀有']
+weights = [80,15,5]
+q = random.choices(cards, k=10, weights=weights)
+print(q)
