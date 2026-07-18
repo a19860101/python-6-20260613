c5f334097849ead6cbfd5ecbfa6d48915b00ae5b
Zac <a19860101@gmail.com>
2026-07-18 10:47:01 +0800

    20260718

--- 0718/requests-3.py
+++ 0718/requests-3.py
@@ -9,10 +9,11 @@ courses = htmlfile.find_all('div',class_='course-item')
 
 for course in courses:
     title = course.find('h5',class_='card-title').text
-    duration = course.find('small',class_='text-muted').text
+    # duration = course.find('small',class_='text-muted').text
+    duration = course.select_one('.info-row:nth-of-type(2) small').text
     end = course.find('small',class_='text-success').text
 
     print(title)
     print(duration)
-    print(end)
+    print(end.strip())
     print('-'*100)
