5f801fce67dc7cc3258b5798c1bbd08fb52992c5
Zac <a19860101@gmail.com>
2026-07-18 11:36:36 +0800

    20260718

--- 0718/requests-3.py
+++ 0718/requests-3.py
@@ -1,5 +1,10 @@
 import requests
 import bs4
+import openpyxl
+
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+
 url = 'https://www.taoyuancollege.com.tw/all-courses?category=employed'
 response = requests.get(url)
 htmlfile = bs4.BeautifulSoup(response.text,'html.parser')
@@ -7,7 +12,7 @@ htmlfile = bs4.BeautifulSoup(response.text,'html.parser')
 
 courses = htmlfile.find_all('div',class_='course-item')
 
-for course in courses:
+for i,course in enumerate(courses):
     #例外處理
     try:
         title = course.find('h5',class_='card-title').text
@@ -20,3 +25,6 @@ for course in courses:
     print(duration)
     print(end.strip())
     print('-'*100)
+    sheet.append([i+1,title, duration, end.strip()])
+
+workbook.save('test.xlsx')
