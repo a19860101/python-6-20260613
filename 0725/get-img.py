e302511b67f4445c8abeb026e109bf9dd0bf0eff
Zac <a19860101@gmail.com>
2026-07-25 11:01:57 +0800

    20260725

--- 0725/get-img.py
+++ 0725/get-img.py
@@ -1,7 +1,11 @@
 import os
+import re
 import requests
 import bs4
 import urllib.request as req
+
+
+# 正規表達式 re
 
 url = 'https://www.tenlong.com.tw/zh_tw/recent'
 
@@ -13,6 +17,8 @@ os.makedirs('img', exist_ok=True)
 books = htmlfile.find_all('li', class_='single-book')
 for i,book in enumerate(books):
     title = book.select_one('.title a').text
+    title = re.sub(r'[\\/?<>:|]','_',title)
+    title = re.sub(r'[\x00-\x1f]','',title)
     img_url = book.find('img')['src']
     print(img_url)
-    req.urlretrieve(img_url, f'img/{i}.jpg')
+    req.urlretrieve(img_url, f'img/{title}.jpg')
