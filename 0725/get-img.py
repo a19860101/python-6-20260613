c1bd40bac9f9b04ddde8406d2fbf790f0d865f85
Zac <a19860101@gmail.com>
2026-07-25 10:22:08 +0800

    20260725

--- 0725/get-img.py
+++ 0725/get-img.py
@@ -1,5 +1,7 @@
+import os
 import requests
 import bs4
+import urllib.request as req
 
 url = 'https://www.tenlong.com.tw/zh_tw/recent'
 
@@ -7,9 +9,10 @@ response = requests.get(url)
 htmlfile = bs4.BeautifulSoup(response.text, 'html.parser')
 
 # print(htmlfile)
-
+os.makedirs('img', exist_ok=True)
 books = htmlfile.find_all('li', class_='single-book')
-for book in books:
+for i,book in enumerate(books):
     title = book.select_one('.title a').text
     img_url = book.find('img')['src']
     print(img_url)
+    req.urlretrieve(img_url, f'img/{i}.jpg')
