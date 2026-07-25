2a35463b4915d50a5e7bcfce99bafab572e4baf0
Zac <a19860101@gmail.com>
2026-07-25 15:24:40 +0800

    20260725

--- 0725/selenium-nike-1.py
+++ 0725/selenium-nike-1.py
@@ -7,12 +7,16 @@ driver = webdriver.Chrome()
 url = 'https://www.nike.com/tw/w/sale-3yaep'
 driver.get(url)
 driver.maximize_window()
-time.sleep(5)
+time.sleep(2)
 
-titles = driver.find_elements(By.CLASS_NAME, 'product-card__title')
+products = driver.find_elements(By.CLASS_NAME, 'product-card__body')
 # print(titles)
-for title in titles:
-    print(title)
+for product in products:
+    title = product.find_element(By.CLASS_NAME, 'product-card__title').text
+    price = product.find_element(By.CLASS_NAME, 'is--current-price').text
+# is--current-price
+    print(f'商品名稱:{title} \n 目前售價:{price}')
+    print('*'*30)
 
 time.sleep(2)
 
