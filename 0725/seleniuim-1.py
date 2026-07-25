acf5796d2ef96d7f751b6734eb6dc12d0d4fd571
Zac <a19860101@gmail.com>
2026-07-25 14:17:44 +0800

    20260725

--- 0725/seleniuim-1.py
+++ 0725/seleniuim-1.py
@@ -1,9 +1,27 @@
 from selenium import webdriver
 import time
 
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
 driver = webdriver.Chrome()
 
 driver.get("https://www.google.com/")
+time.sleep(1)
+search = driver.find_element(By.CLASS_NAME, 'gLFyf')
+search.send_keys('h')
+time.sleep(0.6)
+search.send_keys('e')
+time.sleep(0.2)
+search.send_keys('l')
+time.sleep(0.3)
+search.send_keys('l')
+time.sleep(0.25)
+search.send_keys('o')
+time.sleep(0.65)
+
+search.send_keys(Keys.ENTER)
+
 driver.save_screenshot('test.png')
 time.sleep(2)
 
