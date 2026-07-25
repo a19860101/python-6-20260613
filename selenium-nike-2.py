4e71fb4f3bd3e25bd029f382b47f138ff3c30021
Zac <a19860101@gmail.com>
2026-07-25 16:08:23 +0800

    20260725

--- selenium-nike-2.py
+++ selenium-nike-2.py
@@ -8,17 +8,23 @@ url = 'https://www.nike.com/tw/w/sale-3yaep'
 driver.get(url)
 driver.maximize_window()
 time.sleep(2)
-products = driver.find_elements(By.CLASS_NAME, 'product-card__body')
+count = 0
+while True:
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight - 1500)")
+    time.sleep(2)
+    products = driver.find_elements(By.CLASS_NAME, 'product-card__body')
+    print(len(products))
+    if len(products) == count:
+        break
+    count = len(products)
+# products = driver.find_elements(By.CLASS_NAME, 'product-card__body')
 for product in products:
     title = product.find_element(By.CLASS_NAME, 'product-card__title').text
     price = product.find_element(By.CLASS_NAME, 'is--current-price').text
     print(f'商品名稱:{title} \n 目前售價:{price}')
     print('*'*30)
-    driver.execute_script("window.scrollTo(0,3000)")
+#     driver.execute_script("window.scrollTo(0,3000)")
 
-time.sleep(2)
-
-driver.quit()
 time.sleep(2)
 
 driver.quit()
