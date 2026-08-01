ef64d70ebeeb43aaa09cc27e5be76b9efba165a9
Zac <a19860101@gmail.com>
2026-08-01 11:42:13 +0800

    20260801

--- 0801/selenium-apple-1.py
+++ 0801/selenium-apple-1.py
@@ -2,20 +2,37 @@ from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import time
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 url = 'https://www.apple.com/tw/shop/refurbished/mac'
 driver = webdriver.Chrome()
 driver.get(url)
 driver.maximize_window()
 
-time.sleep(2)
-products = driver.find_elements(By.CLASS_NAME, 'rf-refurb-producttiles')
-for product in products:
-    print(product.text)
+all_products = []
 
-driver.find_element(By.XPATH, '//*[@id="root"]/div/nav/div/div[3]/button').click()
-time.sleep(5)
-
-
+while True:
+    products = driver.find_elements(By.CLASS_NAME, 'rf-refurb-producttile')
+    for product in products:
+        title = product.find_element(By.CLASS_NAME, 'rf-refurb-producttile-link').text
+        price = product.find_element(By.CSS_SELECTOR, 'span.rf-refurb-producttile-currentprice').text
+        p = {}
+        p['title'] = title
+        p['price'] = price
+        all_products.append(p)
+    btn = driver.find_element(By.XPATH, '//*[@id="root"]/div/nav/div/div[3]/button')
+    if btn.get_attribute('disabled'):
+        break
+    btn.click()
+    time.sleep(2)
+print(f'共找到{len(all_products)}筆整修品')
+for product in all_products:
+    print(f'商品名稱：{product['title']}')
+    print(f'價格：{product['price']}')
+    print('*' * 100)
+# print(len(all_products))
 
 driver.quit()
+
