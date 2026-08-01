3061e1cdcb7473224510fea5ca27f19b045b7822
Zac <a19860101@gmail.com>
2026-08-01 14:56:02 +0800

    20260801

--- 0801/tg.py
+++ 0801/tg.py
@@ -2,5 +2,80 @@ import os
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import (
-    ApplicationBuilder
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
 )
+
+
+#########################################
+# 讀取.env
+load_dotenv()
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+############################################
+# 處理 /start 指令
+async def start_command(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+    await update.message.reply_text(
+        "你好，我是 RSS激起人！\n"
+        "你可以傳送任何文字給我。"
+    )
+
+######################################################
+# 處理一般文字訊息
+async def echo_message(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+    # 取得使用者輸入的文字
+    user_text = update.message.text
+
+    print("使用者輸入：", user_text)
+
+    # 將相同的文字回覆給使用者
+    await update.message.reply_text(
+        f"你說了：{user_text}"
+    )
+
+##############################################
+# 主程式
+def main():
+    if not TELEGRAM_BOT_TOKEN:
+        print("找不到TOKEN")
+        print("請檢查.env")
+        return
+
+    # 建立Telegram Bot應用程式
+    application = (
+        ApplicationBuilder()
+        .token(TELEGRAM_BOT_TOKEN)
+        .build()
+    )
+
+    # 加入 /start指令處理器
+    application.add_handler(
+        CommandHandler("start", start_command)
+    )
+
+    # 加入一般文字訊息處理器
+    application.add_handler(
+        MessageHandler(
+            filters.TEXT & ~filters.COMMAND,
+            echo_message
+        )
+    )
+
+    print("Telegram Bot 已啟動")
+    print("按下 Ctrl + C 可以停止程式")
+
+    # 持續接收 Telegram 訊息
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
