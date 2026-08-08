8554c4412fdbe47e13d74c1593e50ba4ee23e09b
Zac <a19860101@gmail.com>
2026-08-08 11:59:53 +0800

    20260808

--- 0808/stocks-3.py
+++ 0808/stocks-3.py
@@ -2,9 +2,22 @@ import yfinance
 import mplfinance as mpl
 import matplotlib.pyplot as plt
 
-code = '0050.TW'
-result = yfinance.download(code, period='3mo')
+code = 'AAPL'
+# result = yfinance.download(code, period='1mo', auto_adjust=False)
+result = yfinance.download(code, start='2025-06-01' , end='2025-6-30', auto_adjust=False)
 print(result.columns)
 result.columns = result.columns.get_level_values(0)
 print(result.columns)
-mpl.plot(result, type='candle')
+
+market_color = mpl.make_marketcolors(
+    up='red',
+    down='green',
+    inherit=True
+)
+
+style = mpl.make_mpf_style(
+    marketcolors=market_color,
+)
+
+
+mpl.plot(result, type='candle', style=style)
