cd2bc98c1d7b1cdbe216277ffbfc8d03fbea8f22
Zac <a19860101@gmail.com>
2026-08-08 10:18:10 +0800

    20260808

--- 0808/stocks-1.py
+++ 0808/stocks-1.py
@@ -1,9 +1,21 @@
 import yfinance
+import matplotlib.pyplot as plt
 
 # code = ['0050.TW','0056.TW']
 
-code = input('請輸入台股代碼：')
+# code = input('請輸入台股代碼：')
 
-result = yfinance.download(f'{code}.TW', period='1mo')
+code = '0050.TW'
 
-print(result)
+result = yfinance.download(code, period='1mo')
+# result = yfinance.download(code, start='2026-01-01', end='2026-08-07')
+
+# print(result)
+plt.plot(result.index, result['High'])
+plt.plot(result.index, result['Low'])
+plt.plot(result.index, result['Close'])
+
+plt.show()
+
+
+
