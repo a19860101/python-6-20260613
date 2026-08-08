c39996eb8eb9d8841b6ace847d15e946e4afcfe3
Zac <a19860101@gmail.com>
2026-08-08 12:15:39 +0800

    20260808

--- 0808/stocks-3.py
+++ 0808/stocks-3.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 code = 'AAPL'
 # result = yfinance.download(code, period='1mo', auto_adjust=False)
 result = yfinance.download(code, start='2025-06-01' , end='2025-6-30', auto_adjust=False)
+# auto_adjust=False
+# 使用原始股價
+# auto_adjust=True
+# 使用調整後股價
 print(result.columns)
 result.columns = result.columns.get_level_values(0)
 print(result.columns)
