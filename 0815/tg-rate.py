99e97404b390b79ac8e480e7f958cf8d48026cba
Zac <a19860101@gmail.com>
2026-08-15 12:00:53 +0800

    20260815

--- 0815/tg-rate.py
+++ 0815/tg-rate.py
@@ -11,7 +11,10 @@ update_id = 0
 response = requests.get(GET_UPDATES_URL)
 msg = response.json()
 print(msg)
-for update in msg['result']:
-    print(update['message']['text'])
-    print(update['message']['chat']['id'])
-    print(update['update_id'])
+chat_id = msg['result'][0]['message']['chat']['id']
+print(chat_id)
+send_msg = requests.post(SEND_MESSAGES_URL,data={'chat_id':chat_id,'text':'HELLOOOOOOOO!!!'})
+# for update in msg['result']:
+#     print(update['message']['text'])
+#     print(update['message']['chat']['id'])
+#     print(update['update_id'])
