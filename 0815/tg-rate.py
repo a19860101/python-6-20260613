6ce65a5ea96225249247dd607c4e2d3edeb6df81
Zac <a19860101@gmail.com>
2026-08-15 11:44:57 +0800

    20260815

--- 0815/tg-rate.py
+++ 0815/tg-rate.py
@@ -7,3 +7,11 @@ GET_UPDATES_URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 SEND_MESSAGES_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 update_id = 0
+
+response = requests.get(GET_UPDATES_URL)
+msg = response.json()
+print(msg)
+for update in msg['result']:
+    print(update['message']['text'])
+    print(update['message']['chat']['id'])
+    print(update['update_id'])
