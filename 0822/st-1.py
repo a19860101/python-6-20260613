01385b9c8455c5babb5eb57d83114bf2f0932074
Zac <a19860101@gmail.com>
2026-08-22 09:59:50 +0800

    20260822

--- 0822/st-1.py
+++ 0822/st-1.py
@@ -4,3 +4,12 @@ st.title('Hello World!!! 123')
 st.header('Header')
 st.subheader('Subheader')
 st.write('normal text')
+
+name = st.text_input('請輸入姓名：')
+currency = st.selectbox(
+    '選項',
+    ['USD','JPY','EUR']
+)
+if st.button('輸入'):
+    st.write(name)
+    st.write(currency)
