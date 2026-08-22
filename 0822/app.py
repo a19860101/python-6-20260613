8632308ea76b6760ebaed148ea3220f4f78a6edb
Zac <a19860101@gmail.com>
2026-08-22 13:44:32 +0800

    20260822

--- 0822/app.py
+++ 0822/app.py
@@ -3,7 +3,8 @@ import streamlit as st
 home = st.Page('home.py', title='首頁')
 rate = st.Page('rate.py',title='取得匯率')
 rss = st.Page('rss.py',title='取得RSS')
+weather = st.Page('weather.py', title='天氣資訊')
 
-pg = st.navigation([home, rate, rss])
+pg = st.navigation([home, rate, rss, weather])
 
 pg.run()
